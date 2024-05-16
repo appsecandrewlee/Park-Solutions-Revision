a3d253b146875cc359d41a5c1c39549099c05606
appsecandrewlee <appsec.andrewlee@gmail.com>
2024-05-17 02:59:50 +1000

    added wobble cards

--- parksol/app/page.tsx
+++ parksol/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/hero";
 import { ParallaxScrollSecondDemo } from "@/components/product";
 import Tags from "@/components/tag";
 import { TypewriterEffectSmoothDemo } from "../components/process";
+import { WobbleCardDemo } from "@/components/certificate";
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
@@ -10,6 +11,7 @@ export default function Home() {
       <ParallaxScrollSecondDemo />
       <Tags />
       <TypewriterEffectSmoothDemo />
+      <WobbleCardDemo />
     </main>
   );
 }
---
+++ parksol/components/certificate.tsx
@@ -0,0 +1,56 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import { WobbleCard } from "./ui/wobble-card";
+
+export function WobbleCardDemo() {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full">
+      <WobbleCard
+        containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[500px] lg:min-h-[300px]"
+        className=""
+      >
+        <div className="max-w-xs">
+          <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            Park Solutions
+          </h2>
+          <p className="mt-4 text-left  text-base/6 text-neutral-200">
+            One of the biggest development in commerce chain for park essentials{" "}
+          </p>
+        </div>
+        <Image
+          src="/product.png"
+          width={1200}
+          height={800}
+          alt="linear demo image"
+          className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl"
+        />
+      </WobbleCard>
+      <WobbleCard containerClassName="col-span-1 min-h-[300px]">
+        <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+          When it comes to warner brother collaboration
+        </h2>
+        <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+          We are not kidding
+        </p>
+      </WobbleCard>
+      <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
+        <div className="max-w-sm">
+          <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            This is going very well for the development process
+          </h2>
+          <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+            we always have more customers{" "}
+          </p>
+        </div>
+        <Image
+          src="/linear.webp"
+          width={500}
+          height={500}
+          alt="linear demo image"
+          className="absolute -right-10 md:-right-[40%] lg:-right-[20%] -bottom-10 object-contain rounded-2xl"
+        />
+      </WobbleCard>
+    </div>
+  );
+}
