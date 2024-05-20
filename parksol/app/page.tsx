eeb8a9d03ef082e4c86581d77d166b01cb042b4b
appsecandrewlee <appsec.andrewlee@gmail.com>
2024-05-20 16:44:07 +1000

    initial deployment try

--- parksol/app/page.tsx
+++ parksol/app/page.tsx
@@ -5,15 +5,18 @@ import { TypeWritingTitle } from "../components/process";
 import { CertificateCards } from "@/components/certificate";
 import { IpadTitle } from "@/components/about";
 import  {Contact}  from "@/components/contactform";
+import { TracingBeam } from "@/components/ui/tracing-beam";
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
+
       <Hero />
       <ProductSection />
       <Tags />
       <TypeWritingTitle />
       <CertificateCards />
       <IpadTitle />
+
     </main>
   );
 }
